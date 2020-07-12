ec87170e718f879417e0196c3d36813c903bf881
Tekena Solomon <tekena157@gmail.com>
2020-07-13 00:51:52 +0100

    added sequelize and cerated migrations

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const cors = require('cors')
-
 const app = express()
 
 app.use(cors())
+app.use(express.json())
+
+require('./database/db/connnection')
 
 module.exports = app
