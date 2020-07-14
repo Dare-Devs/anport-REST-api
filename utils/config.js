a76ebb16a56e59e8da8620acdc94814ef179abe6
Tekena Solomon <tekena157@gmail.com>
2020-07-14 01:49:57 +0100

    added exports of the current dbvalue for config.js

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,17 @@
+const dbconfig = require('../config/config.json')
+
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+
+let dbValues
+
+if (process.env.NODE_ENV === 'production') {
+  dbValues = dbconfig.production
+} else if (process.env.NODE_ENV === 'test') {
+  dbValues = dbconfig.test
+} else {
+  dbValues = dbconfig.development
+}
 
 let PORT = process.env.PORT
 
-module.exports = { PORT }
+module.exports = { PORT, dbValues }
