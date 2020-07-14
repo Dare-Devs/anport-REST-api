66bd9601c666c29b8e487bf2ec2b38dc1d974c93
Tekena Solomon <tekena157@gmail.com>
2020-07-14 01:48:54 +0100

    fixed database connection issue

--- database/db/connnection.js
+++ database/db/connnection.js
@@ -1,9 +1,23 @@
 const Sequelize = require('sequelize')
+const { dbValues } = require('../../utils/config')
 
-const sequelize = new Sequelize('anport', 'root', 'dodo123=', {
-  host: 'localhost',
-  dailect: 'mariadb',
-  operatorsAliases: false,
-})
+const sequelize = new Sequelize(
+  dbValues.database,
+  dbValues.username,
+  dbValues.password,
+  {
+    host: dbValues.host,
+    dialect: 'mysql',
+  }
+)
+const dbCheck = async () => {
+  try {
+    await sequelize.authenticate()
+    console.log('Connection has been established successfully.')
+  } catch (error) {
+    console.error('Unable to connect to the database:', error)
+  }
+}
+dbCheck()
 
 module.exports = sequelize
