efbd4c3ee642126090f63857a9be2a841f01d76f
Tekena Solomon <tekena157@gmail.com>
2020-07-15 08:31:27 +0100

    changed status of sequelize errors to 400

--- utils/middleware.js
+++ utils/middleware.js
@@ -21,11 +21,11 @@ const errorHandler = (error, request, response, next) => {
   } else if (error.name === 'JsonWebTokenError') {
     return response.status(401).json({ error: 'invalid token' })
   } else if (error.name === 'SequelizeUniqueConstraintError') {
-    return response.status(401).json({ error: error.message })
+    return response.status(400).json({ error: error.message })
   } else if (error.name === 'SequelizeValidationError') {
-    return response.status(401).json({ error: error.message })
+    return response.status(400).json({ error: error.message })
   } else if (error.name === 'SequelizeDatabaseError') {
-    return response.status(401).json({ error: error.message })
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
